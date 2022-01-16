13618a710bc87b542c009f8ee610c2aa8edd17c5
pauloedwardo <plo.erd@gmail.com>
2022-01-16 12:39:04 -0300

    Initial Commit.

---
+++ src/main/java/com/poc/blocking/queue/PocBlockingQueueApplication.java
@@ -0,0 +1,13 @@
+package com.poc.blocking.queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocBlockingQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocBlockingQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/blocking/queue/PocBlockingQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.blocking.queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocBlockingQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
